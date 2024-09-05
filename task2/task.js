2ec0a4872f204102139e9d32729eeeb33010dc2d
TheOneAndOnly71 <114691524+TheOneAndOnly71@users.noreply.github.com>
2024-09-05 13:17:05 +0200

    Task 2 complete
    passed all test

--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,24 @@
 export function countDifferentLetters(sentence) {
-
+    let characters = [];
+    let alphabet = [];
+    for (let i = 0; i < 26; i++) {
+        alphabet.push(String.fromCharCode(97 + i))
+    }
+    for(let i = 0;i < sentence.length; i++){
+        if(sentence[i] === sentence[i].toUpperCase()){
+            characters.push(sentence[i])
+            characters.push(sentence[i].toLowerCase())
+        }else{
+            characters.push(sentence[i])
+            characters.push(sentence[i].toUpperCase())
+        }
+    }
+    let letters = characters.filter(element => alphabet.includes(element));
+    const uniqueLetters = letters.reduce((unique, letter) => {
+        if (!unique.includes(letter)) {
+            unique.push(letter);
+        }
+        return unique;
+    }, []);
+    return uniqueLetters.length
 }
